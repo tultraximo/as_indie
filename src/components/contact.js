790526c535418eef88f3f1d8cffced6dbf2716dc
tultraximo <jodeelmoody@gmail.com>
2023-10-17 19:57:21 -0700

    flexbox updated

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contact.css'; // Import the CSS file
+
+const Contact = () => {
+  return (
+    <div className="contact-page">dd
+      {/* <img src="path_to_your_image.jpg" alt="Contact Image" className="centered-image" /> */}
+    </div>
+  );
+}
+
+export default Contact;
